806bf8c3f52fa95dbcde93981bad6ec39d18b362
Alfredina-A <daldinart@gmail.com>
2024-11-25 14:08:50 +0000

    commiting all changes

---
+++ src/index.js
@@ -0,0 +1,4 @@
+let name = prompt("What's your name?")
+let age = prompt("What's your age?")
+let email = prompt("Your Email")
+alert (`Hi ${name}, you are ${age} years old and we will keep in touch via your ${email} `)
